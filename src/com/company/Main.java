cd252e4e1f16017604ebd29d9275683a673132d9
Mazhar-4321 <syedmazharali742@gmail.com>
2022-12-06 15:02:16 +0530

    ADDED: Code to Create Linked List by Adding 30 and 56 to 70

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,10 @@ public class Main {
         list.add(30);
         list.add(70);
         System.out.println(list);
+        List<Integer> list1= new List<>();
+        list1.add(70);
+        list1.add(30);
+        list1.add(56);
+        System.out.println(list1);
     }
 }
